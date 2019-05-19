5023a862abf240743c8363816d0967400ae38ac2
Oldteach74 <49858519+Oldteach74@users.noreply.github.com>
2019-05-19 20:42:39 +0800

    M4 A5

--- GradeBook/UserInterfaces/StartingUserInterface.cs
+++ GradeBook/UserInterfaces/StartingUserInterface.cs
@@ -36,7 +36,7 @@ namespace GradeBook.UserInterfaces
             var parts = command.Split(' ');
             if (parts.Length != 3)
             {
-                Console.WriteLine("Command not valid, Create requires a name and type of grade book.");
+                Console.WriteLine("Command not valid, Create requires a name and type of gradebook.");
                 return;
             }
             var name = parts[1];
@@ -48,7 +48,7 @@ namespace GradeBook.UserInterfaces
                 gradeBook = new RankedGradeBook(name);
             else
             {
-                Console.WriteLine("{0} is not a suppported type of gradebook, please try again", type);
+                Console.WriteLine("{0} is not a suppported type of gradebook, please try again",type);
                 return;
             }
                 
